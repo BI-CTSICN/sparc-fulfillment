6acfd7234853d808a4a9cf32f348bf0c0118281f
ctsiteam <ctsiteam@gmail.com>
2017-10-25 10:18:52 -0400

    initial change

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -4,7 +4,7 @@ set :application, "sparc-fulfillment"
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.5'
 set :repo_url, 'git@github.com:BI-CTSICN/sparc-fulfillment.git'
-set :deploy_to, "/opt/SPARC_DEVELOPMENT/sparc-request-release-2.0.5/sparc-fulfillment-capistrano"
+set :deploy_to, "/opt/SPARC_DEVELOPMENT/sparc-request-release-2.0.5/sparc-fulfillment-capistrano1"
 set :rails_env, "testing"
 server '10.54.13.48', user: 'capistrano', roles: %w{app db web}
 set :branch, "cnmc-v2.6.0"
