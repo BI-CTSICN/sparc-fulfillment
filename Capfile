ffab85f233bad0ed5498197a258067b60fe3a8bb
ctsiteam <ctsiteam@gmail.com>
2017-11-08 14:38:45 -0500

    initial setup

--- Capfile
+++ Capfile
@@ -1,8 +1,8 @@
-# Load DSL and set up stages
 require 'capistrano/setup'
-
-# Include default deployment tasks
 require 'capistrano/deploy'
+
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
 
 # Include tasks from other gems included in your Gemfile
 #
@@ -15,14 +15,14 @@ require 'capistrano/deploy'
 #   https://github.com/capistrano/rails
 #   https://github.com/capistrano/passenger
 #
-require 'capistrano/rvm'
-# require 'capistrano/rbenv'
+#require 'capistrano/rvm'
+require 'capistrano/rbenv'
 # require 'capistrano/chruby'
 # require 'capistrano/bundler'
 # require 'capistrano/rails/assets'
 # require 'capistrano/rails/migrations'
 require 'capistrano/passenger'
-require 'capistrano/delayed-job'
+require 'capistrano/delayed_job'
 require 'capistrano/rails'
 
 # required to deploy with dotenv
--- config/deploy.rb
+++ config/deploy.rb
@@ -19,28 +19,17 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.~
 
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.10.0'
 
-set :application, 'fulfillment'
-set :repo_url, 'git@github.com:bmic-development/sparc-fulfillment.git'
+set :application, "sparc-fulfillment"
+set :repo_url, "git@github.com:BI-CTSICN/sparc-fulfillment.git"
+set :user, 'capistrano'
+set :use_sudo, false
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :stages, %w(testing demo demo2 staging production)
+set :default_stage, 'testing'
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/rails/fulfillment'
-
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/faye.yml', 'config/shards.yml', '.env')
